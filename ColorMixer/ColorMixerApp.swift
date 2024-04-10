fae9667459b73018f8c40c2f9c4b94f33d5f732d
Nicholas-Troyer <nicholastroyer2@gmail.com>
2024-04-10 09:38:51 -0500

    Initial Commit

---
+++ ColorMixer/ColorMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixerApp.swift
+//  ColorMixer
+//
+//  Created by TROYER, NICK on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
