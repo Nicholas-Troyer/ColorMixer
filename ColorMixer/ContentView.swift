adbc8f7ba986e82b5ead30f5d14c9b85787866ee
Nicholas Troyer <128517325+Nicholas-Troyer@users.noreply.github.com>
2024-04-17 08:22:34 -0500

    added new text

--- ColorMixer/ContentView.swift
+++ ColorMixer/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Git!")
+            
+            Text("Goodbye, Git!")
         }
         .padding()
     }
